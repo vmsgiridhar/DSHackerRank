a832ee3070ea1450ec86cc2bcd394cd1254187c4
Trisul <vmsgbtechgiet@gmail.com>
2018-06-14 16:55:41 +0530

    Updating LinkedLists

---
+++ DataStructures_Practice/LinkedLists/InsertNodeAtLast.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 14 15:29:23 2018
+
+@author: C5232886
+"""
+
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.next = None
+    def traverse(self):
+        node = self
+        while node != None:
+            print(node.val)
+            node = node.next
+    def insertNodeAtLast(self,valueOfLastNode):
+        node = self
+        while node != None:
+            temp = node
+            node = node.next
+        newnode = Node(valueOfLastNode)
+        temp.next = newnode
+node1 = Node(1)
+node2 = Node(2)
+node1.next = node2
